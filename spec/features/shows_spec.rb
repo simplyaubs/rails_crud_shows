ddbddc32b29d7d2c765c86ccab0ea97f316a9ebb
Aubrey Howell <simplyaubs@gmail.com>
2014-06-21 20:45:45 -0400

    User can update a show from list.

--- spec/features/shows_spec.rb
+++ spec/features/shows_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD shows' do
     expect(page).to have_content 'Game of Thrones'
     expect(page).to have_content 'Sunday'
   end
+
+  scenario 'User can update a show from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a show'
+    fill_in 'Name', with: 'Game of Thrones'
+    fill_in 'Day', with: 'Sunday'
+    click_on 'Add show'
+    expect(page).to have_content 'Game of Thrones'
+    expect(page).to have_content 'Sunday'
+    click_on 'Game of Thrones'
+    expect(page).to have_content 'Game of Thrones'
+    expect(page).to have_content 'Sunday'
+    click_on 'Edit'
+    fill_in 'Name', with: 'True Blood'
+    fill_in 'Day', with: 'Monday'
+    click_on 'Update show'
+    expect(page).to have_content 'True Blood'
+    expect(page).to have_content 'Monday'
+    expect(page).to_not have_content 'Game of Thrones'
+    expect(page).to_not have_content 'Sunday'
+  end
 end
